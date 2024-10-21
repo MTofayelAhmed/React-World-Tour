3bed122cbb97086ebb84bdbfaaf01dc800de5d79
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-10-21 21:04:36 +0200

    done with button setup

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,27 +1,28 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../country/Country";
-
+import "./country-container.css"
 
 const Countries = () => {
-const [countries, setCountries]= useState([])
+  const [countries, setCountries] = useState([]);
 
-useEffect(()=>{
-fetch("https://restcountries.com/v3.1/all")
-.then(res => res.json())
-.then(data => setCountries(data))
-},[]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-    return (
-        <div>
-            <h3>Countries: {countries.length}</h3>
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
 
-
-            {
-                countries.map(country=> <Country country = {country} key= {country.cca2}></Country>)
-            }
-        </div>
-    );
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country country={country} key={country.cca2}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,12 +1,30 @@
-import "./country.css"
+import { useState } from "react";
+import "./country.css";
 
-const Country = ({country}) => {
-    const {name}= country
-    return (
-        <div className="country">
-            <h5>Country Name: {name?.common}</h5>
-        </div>
-    );
+const Country = ({ country }) => {
+  const { name, flags, population, area, cca3 } = country;
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
+
+  return (
+    <div className="country">
+      <h5>Country Name: {name?.common}</h5>
+      <img src={flags.png} alt="" />
+      <p> Population: {population}</p>
+      <h5> Area: {area}</h5>
+      <p>
+        <small> {cca3} </small>{" "}
+      </p>
+
+      <button onClick={handleVisited}>Visited</button>
+
+      {visited ?  "I have visited this country" : "I have the interest to visit"}
+    </div>
+  );
 };
 
 export default Country;
